6be0296e37d1f285f7a2637619697bc26e646212
jesserizzo <jesse.rizzo@gmail.com>
2018-07-03 19:33:43 -0500

    Move password to config file. Move entities to variables. Change return 'somewhere else' as default in get_status function

--- magic_clock.py
+++ magic_clock.py
@@ -5,15 +5,19 @@ import time
 
 DEBUG_MODE = False
 DELAY = 0.01
+CONFIG = open("config.txt", "r")
+PWD = CONFIG.readline()
+TRACKERS = ["device_tracker.google_maps_115948204649955307306"]
+PROXIMITIES = ["proximity.home"]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-coil_A_1_pin = 6 #  pink
-coil_A_2_pin = 13 #  orange
-coil_B_1_pin = 19 #  blue
-coil_B_2_pin = 26 #  yellow
+coil_A_1_pin = 6
+coil_A_2_pin = 13
+coil_B_1_pin = 19
+coil_B_2_pin = 26
 
-# adjust if different
+
 clockwise = [0, 1, 2, 3, 4, 5, 6, 7]
 clockwise[0] = [0, 0 ,0, 1]
 clockwise[1] = [0, 0, 1, 1]
@@ -34,19 +38,16 @@ counter_clockwise[5] = [0, 0, 1, 1]
 counter_clockwise[6] = [0, 0, 0, 1]
 counter_clockwise[7] = [1, 0, 0, 1]
 
-# GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
 
-# GPIO.output(enable_pin, 1)
-
 
 class Clock():
     def __init__(self):
-        self.jesse_hand = 0
-        self.megan_hand = 0
+        self.hand_1 = 0
+        self.hand_2 = 0
 
 
 def setStep(w1, w2, w3, w4):
@@ -70,71 +71,65 @@ def forward(steps):
             time.sleep(DELAY)
 
 
-def get_jesse_location():
-    """Get Jesse's location from Home Assistant"""
-    url = "http://192.168.1.11:8123/api/states/device_tracker.google_maps"\
-          "_115948204649955307306?api_password=ty2PNJAAHLk4bqU9145h"
+def get_location(tracker):
+    """Get  location from Home Assistant"""
+    url = "http://192.168.1.11:8123/api/states/{}?api_password={}".format(tracker, PWD)
     try:
         response = requests.get(url, timeout=5)
         return (json.loads(response.text)["state"])
     except (requests.exceptions.RequestException, ValueError):
-        print("error getting jesse location")
+        print("error getting location for {}".format(tracker))
         return
 
 
-def get_jesse_travelling():
-    """Get Jesse's travelling status from Home Assistant"""
-    url = "http://192.168.1.11:8123/api/states/proximity.home"\
-          "?api_password=ty2PNJAAHLk4bqU9145h"
+def get_travelling(proximity):
+    """Get travelling status from Home Assistant"""
+    url = "http://192.168.1.11:8123/api/states/{}?api_password={}".format(proximity, PWD)
     try:
         response = requests.get(url, timeout=5)
         return (json.loads(response.text)["attributes"]["dir_of_travel"])
     except (requests.exceptions.RequestException, ValueError):
-        print("error getting jesse travelling")
+        print("error getting travelling status for {}".format(proximity))
         return
 
 
-def get_jesse_status():
-    if DEBUG_MODE is True:
-        return (int(input()))
-    if get_jesse_location() == "home":
+def get_status(tracker, proximity):
+    if get_location(tracker) == "home":
         # return "Home"
         return 0
-    elif get_jesse_location() == "school":
+    elif get_location(tracker) == "school":
         # return "School"
         return 1
-    elif get_jesse_location() == "work":
+    elif get_location(tracker) == "work":
         # return "Work"
         return 2
-    elif get_jesse_location() == "away" and get_jesse_travelling() == \
-            "towards" or get_jesse_travelling() == "away_from":
+    elif get_location(tracker) == "not_home" and get_travelling(proximity) \
+            == "towards" or get_travelling(proximity) == "away_from":
         # return "Travelling"
         return 3
-    elif get_jesse_location() == "away" and get_jesse_travelling() \
-            == "stationary":
-        # return "Somewhere Else"
-        return 4
-    elif get_jesse_location() == "hospital":
+    elif get_location(tracker) == "hospital":
         # return "Hospital"
         return 5
-    elif get_jesse_location() == "prison":
+    elif get_location(tracker) == "prison":
         # return "Prison"
         return 6
-    elif get_jesse_location() == "mortal peril":
+    elif get_location(tracker) == "mortal peril":
         # return "Moral Peril"
         return 7
+    else:
+        # return "Somewhere Else"
+        return 4
 
 
-def update_clock_hand(clock):
+def update_clock_hand(clock, new_position):
     clock = clock
-    new_position = get_jesse_status()
     steps = 0
 
-    if new_position == clock.jesse_hand:
+    if new_position == clock.hand_1:
         pass
     else:
-        steps = new_position - clock.jesse_hand
-        clock.jesse_hand = clock.jesse_hand + steps
+        steps = new_position - clock.hand_1
+        clock.hand_1 = clock.hand_1 + steps
     return steps
 
 
@@ -142,14 +137,15 @@ def __main__():
     clock = Clock()
     try:
         while True:
-            print("current position {}".format(clock.jesse_hand))
-            num_steps = update_clock_hand(clock)
-            if num_steps > 0:
-                forward(num_steps * 64)
-            elif num_steps < 0:
-                backwards(abs(num_steps * 64))
+            for i in len(TRACKERS):
+                new_position = get_status(TRACKERS[i], PROXIMITIES[i])
+                num_steps = update_clock_hand(clock, new_position)
+                if num_steps > 0:
+                    forward(num_steps * 64)
+                elif num_steps < 0:
+                    backwards(abs(num_steps * 64))
 
-            time.sleep(1)
+                time.sleep(1)
     except KeyboardInterrupt:
         setStep(0, 0, 0, 0)
         exit()
