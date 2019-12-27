bd77049898b0c3b5335c6f745b4e68cda4f11ce0
jesserizzo <jesse.rizzo@gmail.com>
2019-12-27 12:12:37 -0600

    Catch all exceptions in clock functions

--- magic_clock.py
+++ magic_clock.py
@@ -33,15 +33,17 @@ class MagicClock:
         try:
             if type(config.ZONES) is str:
                 response = requests.get(config.ZONES, headers=headers, timeout=5)
+                response.raise_for_status()
                 zones = config.zones_accessor(json.loads(response.text))
                 return zones
             else:
                 return config.ZONES
 
-        except (requests.exceptions.RequestException, ValueError, KeyError):
+        except:
             message = "error getting list of zones from {}".format(config.ZONES)
             fileIO.write_log(message)
-            return
+            fileIO.write_log(traceback.format_exc())
+            return []
 
 
     def update_location(self, url_index):
@@ -52,16 +54,19 @@ class MagicClock:
         }
         try:
             response = requests.get(config.LOCATION_URLS[url_index], headers=headers, timeout=5)
+            response.raise_for_status()
             response_json = json.loads(response.text)
             #self.location = response_json["state"]
 
             self.latitude = config.latitude_accessor(response_json)
             self.longitude = config.longitude_accessor(response_json)
             return
-        except (requests.exceptions.RequestException, ValueError, KeyError):
+        except ():
             message = "error getting location for {}".format(config.LOCATION_URLS[url_index])
             fileIO.write_log(message)
+            fileIO.write_log(traceback.format_exc())
             return
+
 
     def update_travelling(self, url_number):
         """Get proximity status from the Home Assistant Api"""
@@ -71,58 +76,69 @@ class MagicClock:
         }
         try:
            response = requests.get(config.LOCATION_URLS[url_number], headers=headers, timeout=5)
+           response.raise_for_status()
            self.travelling = int(json.loads(response.text)["attributes"]["velocity"])
            return
-        except (requests.exceptions.RequestException, ValueError, KeyError):
+        except:
             message = "error getting travelling status for {}".format(config.LOCATION_URLS[url_number])
             fileIO.write_log(message)
+            fileIO.write_log(traceback.format_exc())
             self.travelling = 0
             return
 
 
     def update_zone(self):
         """Iterate through zones and return the first that we are located in."""
-        for zone in self.zones:
-            latitude_meters_diff = abs(zone["latitude"] - self.latitude) * 111139
-            longitude_meters_diff = abs(zone["longitude"] - self.longitude) * 111139
+        try:
+            for zone in self.zones:
+                latitude_meters_diff = abs(zone["latitude"] - self.latitude) * 111139
+                longitude_meters_diff = abs(zone["longitude"] - self.longitude) * 111139
 
-            if latitude_meters_diff < zone["radius"] and longitude_meters_diff < zone["radius"]:
-                self.zone = zone["friendly_name"].lower()
-                return
-        self.zone = None
-        return
+                if latitude_meters_diff < zone["radius"] and longitude_meters_diff < zone["radius"]:
+                    self.zone = zone["friendly_name"].lower()
+                    return
+        except:
+            fileIO.write_log("Error while attempting to find current zone")
+            fileIO.write_log(traceback.format_exc())
+            self.zone = None
+            return
 
 
     def update_hand_position(self):
         """Gets the location and proximity states from Home Assistant and
         returns an int corresponding to location on the clock face"""
+        try:
+            if self.zone == "home":
+                return 4
+            elif (self.travelling >= 5):
+                return 5
+            elif self.zone == "mortal peril":
+                return 0
+            elif self.zone == "friends" and self.travelling < 5:
+                return 1
+            elif self.zone == "family" and self.travelling < 5:
+                return 2
+            elif self.zone == "work" and self.travelling < 5:
+                return 3
+            elif self.zone == "school" and self.travelling < 5:
+                return 6
+            elif self.zone == "hospital" and self.travelling < 5:
+                return 7
+            else:
+                # Point clock hand to "elsewhere".
+                return 8
+        except:
+            fileIO.write_log("Error while attempting to update clock hand position")
+            fileIO.write_log(traceback.format_exc())
 
-        if self.zone == "home":
-            return 4
-        elif (self.travelling >= 5):
-            return 5
-        elif self.zone == "mortal peril":
-            return 0
-        elif self.zone == "friends" and self.travelling < 5:
-            return 1
-        elif self.zone == "family" and self.travelling < 5:
-            return 2
-        elif self.zone == "work" and self.travelling < 5:
-            return 3
-        elif self.zone == "school" and self.travelling < 5:
-            return 6
-        elif self.zone == "hospital" and self.travelling < 5:
-            return 7
-        else:
-            # Point clock hand to "elsewhere".
-            return 8
+def main():
+    fileIO.write_log("Waiting to start program")
+    # hack to give network time to start up when starting program at boot time
+    time.sleep(60)
+    fileIO.write_log("Program started")
 
-
-def __main__():
+    clock = MagicClock()
     try:
-        fileIO.write_log("Program started")
-        clock = MagicClock()
-
         while (True):
             # Iterate through how ever many trackers you have set up
             # Getting the new position and moving the clock hand for each
@@ -145,9 +161,8 @@ def __main__():
         motor.cleanup_GPIO()
         exit()
     except:
+        fileIO.write_log("Unexpected exception in main program loop")
         fileIO.write_log(traceback.format_exc())
-        traceback.print_exc()
 
 
-#if __name__ == "__main__":
-__main__()
+main()
